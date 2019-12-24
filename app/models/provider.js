f957de0d782092395834f49a966adf95e10e3aeb
abdulrahmank <kadkab.abdul@gmail.com>
2019-12-24 08:55:46 +0530

    Project only required fields when shoing all providers

--- app/models/provider.js
+++ app/models/provider.js
@@ -1,15 +1,17 @@
-const BaseModel = require('./base_model')
+const BaseModel = require('./base_model');
+const mongo = require('mongodb');
 
 class Provider extends BaseModel {
     
     async findAll(providerName) {
+        const projections = { projection: { name: 1, services: 1, location: 1 } };
         const query = providerName ? {"name" : new RegExp('.*' + providerName + '.*')} : {};
-        const providers = await this.collection.find(query).toArray();
+        const providers = await this.collection.find(query, projections).toArray();
         return providers;
     }
 
     async find(providerId) {
-        const provider = await this.collection.find({ "_id": providerId }).toArray()
+        const provider = await this.collection.find({ "_id": new mongo.ObjectID(providerId) }).toArray()
         return provider;
     }
 
