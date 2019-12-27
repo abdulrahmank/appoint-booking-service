6e495b0b9a594699b2fe6c5f4fafbef38fa556cb
abdulrahmank <kadkab.abdul@gmail.com>
2019-12-28 00:11:46 +0530

    Add push notification ability to provider page

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const ProvidersController = require('./app/controllers/providers_controller')
 const SlotsController = require('./app/controllers/api/v1/slots_controller')
 const bodyParser = require('body-parser')
 const express = require('express');
+
 const app = express();
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 app.listen(3000, function() {
     console.log('listening on 3000')
