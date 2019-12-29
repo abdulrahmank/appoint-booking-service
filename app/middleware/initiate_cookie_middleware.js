c507925add1ca0b37143964f5e4f0383bc508818
abdulrahmank <kadkab.abdul@gmail.com>
2019-12-29 13:24:06 +0530

    Remove debug statements

--- app/middleware/initiate_cookie_middleware.js
+++ app/middleware/initiate_cookie_middleware.js
@@ -5,7 +5,6 @@ function initiateCookie(request, response, next) {
     const authorizationHeader = request.headers.authorization.substring('Basic '.length, request.headers.authorization.length)
     const authorization = base64decode(authorizationHeader);
     const userName = authorization.split(":")[0];
-    console.log("fsfksadlfk"+userName);
     User.connect().then(() => {
         User.findByName(userName).then((user) => {
             response.cookie('userId', user.id.toString());
